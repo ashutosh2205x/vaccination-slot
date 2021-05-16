3033f360a8b1874ca9b7813bca46703e938102ff
Ashutosh Bharti <ashutosh@headerlabs.com>
2021-05-17 00:09:03 +0530

    minor changes

--- cowin.py
+++ cowin.py
@@ -1,25 +1,21 @@
 import requests
-import pprint
-from requests.auth import HTTPBasicAuth
 import time
-import json
 baseurl = 'https://cdn-api.co-vin.in/api/v2/'
 date = time.localtime()
 datetoday = str(date.tm_mday) +'-'+str(date.tm_mon)+'-'+ str(date.tm_year)
-it_is =False
+is_num =False
 print('Enter pincode : ')
 pincode = input()
-# pincode = str(483501)
 
 try:
     int(pincode)
-    it_is = True
+    is_num = True
 except ValueError:
-    it_is = False
+    is_num = False
 
-if it_is:
+if is_num:
     header_dict = {'Host': 'cdn-api.co-vin.in', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36','Accept-Language':'hi_IN'}
-    r = requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode='+pincode+'&date='+datetoday,headers=header_dict)
+    r = requests.get(baseurl+'appointment/sessions/public/calendarByPin?pincode='+pincode+'&date='+datetoday,headers=header_dict)
 
     if r.status_code==200:
         result = r.json()
